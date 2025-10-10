d093e7023226c163afdbd68910693a48f7b33b01
hassaan141 <m2farooq@uwaterloo.ca>
2025-10-10 01:32:55 -0400

    linting and formatting

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import reactPlugin from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import prettier from 'eslint-config-prettier'
+
+export default [
+  { ignores: ['node_modules', 'build', 'dist', '.expo'] },
+
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+      globals: { ...globals.browser, ...globals.node },
+    },
+    plugins: { react: reactPlugin, 'react-hooks': reactHooks },
+    settings: { react: { version: 'detect' } },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/consistent-type-imports': 'error',
+    },
+  },
+
+  prettier,
+]

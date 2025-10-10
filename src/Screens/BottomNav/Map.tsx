5a5545f8eef09286e20037ac2d1386c1b258e008
hassaan141 <m2farooq@uwaterloo.ca>
2025-10-10 14:20:08 -0400

    chore(screens): scaffold Auth, BottomNav, and Navigation placeholder screens

---
+++ src/Screens/BottomNav/Map.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Map() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Map</Text>
+    </View>
+  )
+}

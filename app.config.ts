f499a50202cc9544f169a528a575450b5f2f1f94
Muhammad Farooqi <m2farooq@uwaterloo.ca>
2025-11-27 00:15:14 +0000

    new changes

--- app.config.ts
+++ app.config.ts
@@ -9,10 +9,14 @@ export default () => ({
       },
     },
     android: {
+      package: 'com.hassaan141.jamaahapp',
       permissions: ['ACCESS_COARSE_LOCATION', 'ACCESS_FINE_LOCATION'],
     },
     extra: {
       EXPO_PUBLIC_API_URL: process.env.EXPO_PUBLIC_API_URL,
+      eas: {
+        projectId: '18f59a83-4081-4b80-b61b-67fc127f5577',
+      },
     },
   },
 })
